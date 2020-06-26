5131a65e262526c5a25edcb4a2552b3221e13eda
cvolo4yzhka <34650243+cvolo4yzhka@users.noreply.github.com>
2020-06-26 11:19:39 +0300

    uppack archive to modem folder

--- compile_modem.sh
+++ compile_modem.sh
@@ -2,9 +2,10 @@ cd mcu
 ./make.sh  "BIRD_A_P0MP2_K37MV1_BSP(LWG_DSDS_COTSX).mak" new
 cd ..
 perl ./mcu/modemRenameCopy.pl ./mcu "BIRD_A_P0MP2_K37MV1_BSP(LWG_DSDS_COTSX)"
-mv ./mcu/temp_modem ./modem_p0
+mv ./mcu/temp_modem ./zte_blade_a476
 
 cd apps
 ./build.sh clean,build,pack all GEN90_USER
 cd ..
-cp ./apps/GEN90_USER_rel.tar.bz2 ./modem_p0/
+tar -C ./zte_blade_a476 -xvjf ./apps/GEN90_USER_rel.tar.bz2 --strip-components 1
+#cp ./apps/GEN90_USER_rel.tar.bz2 ./modem_p0/
